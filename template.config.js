436fd2e48c154237a695886aec086fb857afda61
Dhruv Sakhiya <sakhiya.dhruv@gmail.com>
2024-06-19 19:23:47 +0530

    Template init

---
+++ template.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Placeholder used to rename and replace in files
+  // package.json, index.json, android/, ios/
+  placeholderName: "structure",
+
+  // Directory with template
+  templateDir: "./template_project",
+
+  // Path to script, which will be executed after init
+  postInitScript: "./script.js"
+};
